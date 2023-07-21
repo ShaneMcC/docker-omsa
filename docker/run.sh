4ded34163b9454fc0d69ba21966e7e5fa9ea85b7
Shane Mc Cormack <shanemcc@gmail.com>
2023-07-21 22:50:39 +0100

    Add flag to allow non-certified drives to not alert.

--- docker/run.sh
+++ docker/run.sh
@@ -20,6 +20,14 @@ echo "$OMSA_USER:$OMSA_PASS" | chpasswd
 echo "Allowing ${OMSA_USER} access to openmanage..."
 echo "${OMSA_USER}    *       Administrator" > /opt/dell/srvadmin/etc/omarolemap
 
+if [ "${UNCERTIFIED_DRIVES}" = "1" -o "${UNCERTIFIED_DRIVES}" = "yes" ]; then
+	echo "Allow non-certified drives..."
+	sed -i 's/^NonDellCertifiedFlag=.*$/NonDellCertifiedFlag=no/' /opt/dell/srvadmin/etc/srvadmin-storage/stsvc.ini
+else
+	# Reset to default.
+	sed -i 's/^NonDellCertifiedFlag=.*$/NonDellCertifiedFlag=yes/' /opt/dell/srvadmin/etc/srvadmin-storage/stsvc.ini
+fi
+
 echo "Clearing old tmp files..."
 rm -Rf /tmp/* /var/tmp/*
 
