487c4eb99623d38ca35da50ae1ffeebceff375a3
Shane Mc Cormack <shane@shanemcc.co.uk>
2026-09-20 16:37:18 +0100

    updateDockerfile.sh minor tweaks from shellcheck

--- .github/updateDockerfile.sh
+++ .github/updateDockerfile.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -o pipefail
+
 # File we're updating
 DOCKERFILE="${GITHUB_WORKSPACE}/Dockerfile"
 
-if [ "" = "${GITHUB_WORKSPACE}" ]; then
+if [ -z "${GITHUB_WORKSPACE}" ]; then
 	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 	DOCKERFILE="${SCRIPT_DIR}/../Dockerfile"
 fi;
 
-if [ "" = "${GITHUB_STEP_SUMMARY}" -o "" = "${GITHUB_OUTPUT}" ]; then
+if [ -z "${GITHUB_STEP_SUMMARY}" ] || [ -z "${GITHUB_OUTPUT}" ]; then
 	GITHUB_STEP_SUMMARY="/dev/null"
 	GITHUB_OUTPUT="/dev/stdout"
 fi;
@@ -20,11 +22,11 @@ fi;
 
 # Current Versions
 UPSTREAM="almalinux"
-CURRENT_IMAGE=$(cat "${DOCKERFILE}" | grep -Eo "^FROM ${UPSTREAM}:([^ ]+)" | awk -F: '{print $2}')
-CURRENT_SRVADMIN=$(cat "${DOCKERFILE}" | grep -Eo "srvadmin-all[^ ]*")
-CURRENT_DSU=$(cat "${DOCKERFILE}" | grep -Eo "dell-system-update[^ ]*")
+CURRENT_IMAGE=$(grep -Eo "^FROM ${UPSTREAM}:([^ ]+)" "${DOCKERFILE}" | awk -F: '{print $2}')
+CURRENT_SRVADMIN=$(grep -Eo "srvadmin-all[^ ]*" "${DOCKERFILE}")
+CURRENT_DSU=$(grep -Eo "dell-system-update[^ ]*" "${DOCKERFILE}")
 
-if [ "" == "${CURRENT_IMAGE}" ]; then
+if [ -z "${CURRENT_IMAGE}" ]; then
 	echo "Primary image changed, please update .github/updateDockerfile.sh"
 	exit 1
 fi;
@@ -41,7 +43,7 @@ LATEST_SRVADMIN=$(curl -s -L "${OS_REPO}/repodata/filelists.xml.gz" | gunzip - |
 LATEST_DSU=$(curl -s -L "${OSI_REPO}/repodata/filelists.xml.gz" | gunzip - | grep -iA1 "package.*dell-system-update.*${ARCH}" | grep "<version" | awk -F\" '{print "dell-system-update-" $4 "-" $6}')
 
 if [ "${CURRENT_IMAGE}" != "${LATEST_IMAGE}" ]; then
-	CHANGED_THING+=(\`"${UPSTREAM}:${CURRENT_IMAGE}\` => \`${UPSTREAM}:${LATEST_IMAGE}\`")
+	CHANGED_THING+=("\`${UPSTREAM}:${CURRENT_IMAGE}\` => \`${UPSTREAM}:${LATEST_IMAGE}\`")
 fi;
 
 if [ "${CURRENT_SRVADMIN}" != "${LATEST_SRVADMIN}" ]; then
@@ -52,35 +54,34 @@ if [ "${CURRENT_DSU}" != "${LATEST_DSU}" ]; then
 	CHANGED_THING+=("\`${CURRENT_DSU}\` => \`${LATEST_DSU}\`")
 fi;
 
-if [ "" == "${LATEST_IMAGE}" -o  "" == "${LATEST_SRVADMIN}" -o "" == "${LATEST_DSU}" ]; then
+if [ -z "${LATEST_IMAGE}" ] || [ -z "${LATEST_SRVADMIN}" ] || [ -z "${LATEST_DSU}" ]; then
 	echo "Unable to find new versions."
 	exit 1
 fi;
 
 # Update main image tag
-if [ "${LATEST_IMAGE}" != "" ]; then
-	sed -ri "s/FROM ${UPSTREAM}:[^ ]+/FROM ${UPSTREAM}:${LATEST_IMAGE}/" ${DOCKERFILE}
-fi;
+sed -ri "s/FROM ${UPSTREAM}:[^ ]+/FROM ${UPSTREAM}:${LATEST_IMAGE}/" "${DOCKERFILE}"
 
 # Update our ADD-ed files
-while read LINE; do
-	SPLIT=($LINE)
-	HASH=$(curl -L -s ${SPLIT[1]} | md5sum | awk '{print $1}')
-	FILE=${SPLIT[2]}
+while read -r _ URL FILE _; do
+	if ! HASH=$(curl -L -s -f "${URL}" | md5sum | awk '{print $1}'); then
+		echo "Unable to fetch ${URL}"
+		exit 1
+	fi;
 	NEWFILE=${FILE%%-*}-${HASH}
 
 	ESCAPED_FILE=$(printf '%s\n' "$FILE" | sed -e 's/[\/&]/\\&/g')
 	ESCAPED_NEWFILE=$(printf '%s\n' "$NEWFILE" | sed -e 's/[\/&]/\\&/g')
 
-	sed -i "s/${ESCAPED_FILE}/${ESCAPED_NEWFILE}/g" ${DOCKERFILE}
+	sed -i "s/${ESCAPED_FILE}/${ESCAPED_NEWFILE}/g" "${DOCKERFILE}"
 
 	if [ "${FILE}" != "${NEWFILE}" ]; then
 		CHANGED_THING+=("\`${FILE}\` => \`${NEWFILE}\`")
 	fi;
-done < <(cat "${DOCKERFILE}" | grep "^ADD http")
+done < <(grep "^ADD http" "${DOCKERFILE}")
 
 # Update `dnf install`` command
-sed -ri 's/dnf -y install srvadmin-all[^ ]* dell-system-update[^ ]*/dnf -y install '"${LATEST_SRVADMIN}"' '"${LATEST_DSU}"'/' ${DOCKERFILE}
+sed -ri 's/dnf -y install srvadmin-all[^ ]* dell-system-update[^ ]*/dnf -y install '"${LATEST_SRVADMIN}"' '"${LATEST_DSU}"'/' "${DOCKERFILE}"
 
 # Has Changed?
 echo ""
@@ -88,20 +89,20 @@ git --no-pager diff "${DOCKERFILE}"
 git diff-files --quiet "${DOCKERFILE}"
 CHANGED=${?}
 
-if [ $CHANGED != 0 ]; then
-	echo "**\`Dockerfile\` was changed**" | tee -a $GITHUB_STEP_SUMMARY
+if [ "${CHANGED}" != 0 ]; then
+	echo "**\`Dockerfile\` was changed**" | tee -a "${GITHUB_STEP_SUMMARY}"
 
-	echo "" | tee -a $GITHUB_STEP_SUMMARY
+	echo "" | tee -a "${GITHUB_STEP_SUMMARY}"
 	for THING in "${CHANGED_THING[@]}"; do
-	     echo " - " $THING | tee -a $GITHUB_STEP_SUMMARY
+	     echo " - " "${THING}" | tee -a "${GITHUB_STEP_SUMMARY}"
 	done
 	ALL_CHANGED_THINGS=$(printf ", %s" "${CHANGED_THING[@]}")
 	ALL_CHANGED_THINGS=${ALL_CHANGED_THINGS:2}
 
-	echo "changes_detected=true" >> $GITHUB_OUTPUT
-	echo "changed_items=${ALL_CHANGED_THINGS}" >> $GITHUB_OUTPUT
+	echo "changes_detected=true" >> "${GITHUB_OUTPUT}"
+	echo "changed_items=${ALL_CHANGED_THINGS}" >> "${GITHUB_OUTPUT}"
 else
-	echo "No changes detected" | tee -a $GITHUB_STEP_SUMMARY
-	echo "changes_detected=false" >> $GITHUB_OUTPUT
-	echo "changed_items=" >> $GITHUB_OUTPUT
+	echo "No changes detected" | tee -a "${GITHUB_STEP_SUMMARY}"
+	echo "changes_detected=false" >> "${GITHUB_OUTPUT}"
+	echo "changed_items=" >> "${GITHUB_OUTPUT}"
 fi;
